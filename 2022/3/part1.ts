07f296e4356556119916d16b1677790115545f98
Felix Kirchhoff <felix.kirchhoff@neuefische.de>
2022-12-06 23:36:05 +0100

    fix stuff and add day 3

--- 2022/3/part1.ts
+++ 2022/3/part1.ts
@@ -7,16 +7,28 @@ function getInput(currentChallengeIndex: number): string[] {
     .split("\n");
 }
 
-console.log("test");
+function getOverlap(string1: string, string2: string): string {
+  return [...string1].find((char) => string2.includes(char));
+}
 
 const input = getInput(3);
 const rucksacks = input.map((string) => ({
   c1: string.substring(0, string.length / 2),
   c2: string.substring(string.length / 2),
 }));
+const wrongItems = rucksacks.map(({ c1, c2 }) => getOverlap(c1, c2));
 
-console.log(
-  rucksacks.every((rucksack) => rucksack.c1.length === rucksack.c2.length)
+const sumOfPriorities = wrongItems.reduce(
+  (sum: number, char: string): number => {
+    const lowerCase = new RegExp("[a-z]");
+    if (lowerCase.test(char)) {
+      return sum + char.charCodeAt(0) - 96;
+    } else {
+      return sum + char.charCodeAt(0) - 38;
+    }
+  },
+  0
 );
 
+console.log(sumOfPriorities);
 export {};
--- 2022/3/part2.ts
+++ 2022/3/part2.ts
@@ -1,3 +1,49 @@
 import * as fs from "fs";
 
+import { InputFiles } from "../../node_modules/typescript/lib/typescript";
+
+function getInput(currentChallengeIndex: number): string[] {
+  return fs
+    .readFileSync("./2022/" + currentChallengeIndex + "/input.txt")
+    .toString()
+    .split("\n");
+}
+
+function getOverlap(string1: string, string2: string, string3: string): string {
+  return [...string1].find(
+    (char) => string2.includes(char) && string3.includes(char)
+  );
+}
+
+const input = getInput(3);
+let groups = [];
+
+for (let index = 0; index < input.length; index += 3) {
+  groups.push({
+    bag1: input[index],
+    bag2: input[index + 1],
+    bag3: input[index + 2],
+  });
+}
+const groupBadges = groups.map(({ bag1, bag2, bag3 }) =>
+  getOverlap(bag1, bag2, bag3)
+);
+
+console.log(groupBadges);
+
+const sumOfPriorities = groupBadges.reduce(
+  (sum: number, char: string): number => {
+    const lowerCase = new RegExp("[a-z]");
+    if (lowerCase.test(char)) {
+      return sum + char.charCodeAt(0) - 96;
+    } else {
+      return sum + char.charCodeAt(0) - 38;
+    }
+  },
+  0
+);
+
+console.log(sumOfPriorities);
+export {};
+
 export {};
